1da5e4b415fb2ef744dbabf1784656f38f1ceb80
MatiETorres <136382262+MatiETorres@users.noreply.github.com>
2025-12-30 18:40:11 -0300

    Delete src directory

--- src/models/Pet.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const petSchema = new mongoose.Schema({
-    name: String,
-    type: String,
-    age: Number
-});
-
-export default mongoose.model('Pet', petSchema);
